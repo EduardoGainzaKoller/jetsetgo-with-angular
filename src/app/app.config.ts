3b8032af1b11616bc2fe86f0bb9f986e6783ad5b
Eduardo <edaniel2004gainza@gmail.com>
2025-04-30 00:24:27 +0100

    Basic configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import {environment} from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideFirestore(() => getFirestore())],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAa7rELsPz3KR_stYKtmShlkmn_AWsvAmQ",
+    authDomain: "examenpwm-93504.firebaseapp.com",
+    projectId: "examenpwm-93504",
+    storageBucket: "examenpwm-93504.firebasestorage.app",
+    messagingSenderId: "504956980845",
+    appId: "1:504956980845:web:4da4cb0806b6add6d1f964"
+  },
+};
