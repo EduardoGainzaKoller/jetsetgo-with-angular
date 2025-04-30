63cc0bcd2de07c968fe19ddcfb2c93ac5aca3385
Alvaro <alvarorodmi7@gmail.com>
2025-04-30 18:11:12 +0100

    Filter component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FlightComponent} from './components/flight/flight.component';
+import {FilterComponent} from './components/filter/filter.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FlightComponent, FilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
