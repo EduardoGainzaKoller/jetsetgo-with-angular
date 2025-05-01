812bfe0f5f03f61211314d22c430c3dc1615d5ac
Alberto Redondo <albertoredondoalvarez@gmail.com>
2025-05-01 18:26:44 +0100

    Home pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeFlightsComponent} from './pages/home-flights/home-flights.component';
+import {HomeHotelsComponent} from './pages/home-hotels/home-hotels.component';
+import {HomeCarsComponent} from './pages/home-cars/home-cars.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: 'home-flights', component: HomeFlightsComponent },
+  {path: 'home-hotels', component: HomeHotelsComponent },
+  {path: 'home-cars', component: HomeCarsComponent },
+  {path: '', component: HomeHotelsComponent }
+];
