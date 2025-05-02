c47834f846a03401d99f68b1bbab6f13d8b48de3
Alberto Redondo <albertoredondoalvarez@gmail.com>
2025-05-03 00:00:05 +0100

    Footer routes added and card modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import {HotelSearchComponent} from './pages/hotel-search/hotel-search.component'
 import {CarSearchComponent} from './pages/car-search/car-search.component';
 import {FlightSearchComponent} from './pages/flight-search/flight-search.component';
 import {RegisterComponent} from './pages/register/register.component';
+import {SupportComponent} from './pages/support/support.component';
+import {FrecuencyQuestionsComponent} from './pages/frecuency-questions/frecuency-questions.component';
+import {InformationComponent} from './pages/information/information.component';
 
 
 export const routes: Routes = [
@@ -16,5 +19,8 @@ export const routes: Routes = [
   {path: 'car-search', component: CarSearchComponent },
   {path: 'flight-search', component: FlightSearchComponent },
   {path: 'register', component: RegisterComponent },
+  {path: 'support', component: SupportComponent },
+  {path: 'frequency-questions', component: FrecuencyQuestionsComponent },
+  {path: 'information', component: InformationComponent },
   {path: '', component: HomeHotelsComponent }
 ];
